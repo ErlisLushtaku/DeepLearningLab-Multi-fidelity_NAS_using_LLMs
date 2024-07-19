facd97f9dad79b59dfd366f70f6b9dcbcfd68314
Artin <artinsermaxhaj18@gmail.com>
2024-07-19 13:32:49 +0200

    fix variable key bug

--- utils.py
+++ utils.py
@@ -5,18 +5,28 @@ def convert_LLAMBO_df_to_synetune_dict(config):
     config = config.to_dict()
     dicts = []
     length = len(config['op_0_to_1'])
-    for i in range(length):
+    if length == 1:
+        key = list(config['op_0_to_1'].keys())[0]
         original_dict = {
-            'hp_x0': config['op_0_to_1'][i],
-            'hp_x1': config['op_0_to_2'][i],
-            'hp_x2': config['op_0_to_3'][i],
-            'hp_x3': config['op_1_to_2'][i],
-            'hp_x4': config['op_1_to_3'][i],
-            'hp_x5': config['op_2_to_3'][i]
+            'hp_x0': config['op_0_to_1'][key],
+            'hp_x1': config['op_0_to_2'][key],
+            'hp_x2': config['op_0_to_3'][key],
+            'hp_x3': config['op_1_to_2'][key],
+            'hp_x4': config['op_1_to_3'][key],
+            'hp_x5': config['op_2_to_3'][key]
         }
-        if length == 1:
-            return original_dict
-        dicts.append(original_dict)
+        return original_dict
+    else:
+        for i in range(length):
+            original_dict = {
+                'hp_x0': config['op_0_to_1'][i],
+                'hp_x1': config['op_0_to_2'][i],
+                'hp_x2': config['op_0_to_3'][i],
+                'hp_x3': config['op_1_to_2'][i],
+                'hp_x4': config['op_1_to_3'][i],
+                'hp_x5': config['op_2_to_3'][i]
+            }
+            dicts.append(original_dict)
 
     return dicts
 
